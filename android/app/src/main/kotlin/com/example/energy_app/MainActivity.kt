f6a88d33d58463033efb72f8605bc9ef29c8ed27
RanasingheDD <dishand398@gmail.com>
2024-11-12 13:37:05 +0530

    new commit

--- android/app/src/main/kotlin/com/example/energy_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.energy_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/data/card_data.dart
+++
@@ -1,8 +0,0 @@
-import 'package:energy_app/models/card_model.dart';
-
-List <CardData> cards = [
-CardData(title: "Voltage", image_url: "image_url", value: 230),
-CardData(title: "Current", image_url: "image_url", value: 10),
-CardData(title: "Humadity", image_url: "image_url", value: 1),
-CardData(title: "Power", image_url: "image_url", value: 1)
-];
