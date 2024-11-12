3b82ed3f4746d0472ed55e2e1a602bb0882fd6df
blackdevil <madushanchathuranghe192@gmail.com>
2024-11-12 21:45:17 +0530

    home_page change

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,8 +2,26 @@ import 'package:energy_app/data/card_data.dart';
 import 'package:energy_app/widgets/card.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _showTitle = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // Delay the title appearance for the animation effect
+    Future.delayed(const Duration(milliseconds: 100), () {
+      setState(() {
+        _showTitle = true;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,17 +29,36 @@ class HomePage extends StatelessWidget {
       backgroundColor: const Color.fromARGB(255, 21, 17, 37),
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 21, 17, 37),
-        leading: InkWell(
-          child: IconButton(
-            onPressed: () {
-              ////////////////////
-            },
-            icon: const Icon(
-              Icons.menu,
-              color: Colors.white,
-            ),
+        leading: IconButton(
+          onPressed: () {
+            ////////////////////
+          },
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.white,
           ),
         ),
+        title: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 800),
+          transitionBuilder: (Widget child, Animation<double> animation) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+          child: _showTitle
+              ? const Text(
+                  'Energy Audit Dashboard',
+                  key: ValueKey("title"), // Key triggers AnimatedSwitcher
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              : const SizedBox(), // Empty widget before the title appears
+        ),
+        centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
