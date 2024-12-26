da0290e1543d94dfbb3c91fa3ccc5481f439bb00
Dishan Ranasinghe <162302275+RanasingheDD@users.noreply.github.com>
2024-12-26 14:03:56 +0530

    .

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -106,110 +106,113 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey, // Assign the GlobalKey
-      backgroundColor: const Color.fromARGB(255, 21, 17, 37),
-      appBar: AppBar(
+    return RefreshIndicator(
+      onRefresh: _loadSensorData,
+      child: Scaffold(
+        key: _scaffoldKey, // Assign the GlobalKey
         backgroundColor: const Color.fromARGB(255, 21, 17, 37),
-        leading: IconButton(
-          onPressed: () => _scaffoldKey.currentState?.openDrawer(),
-          icon: const Icon(Icons.menu, color: Colors.white),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 21, 17, 37),
+          leading: IconButton(
+            onPressed: () => _scaffoldKey.currentState?.openDrawer(),
+            icon: const Icon(Icons.menu, color: Colors.white),
+          ),
+          title: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 800),
+            transitionBuilder: (child, animation) =>
+                FadeTransition(opacity: animation, child: child),
+            child: _showTitle
+                ? const Text(
+                    'Energy Audit Dashboard',
+                    key: ValueKey("title"),
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  )
+                : const SizedBox(height: 20,),
+          ),
+          centerTitle: true,
         ),
-        title: AnimatedSwitcher(
-          duration: const Duration(milliseconds: 800),
-          transitionBuilder: (child, animation) =>
-              FadeTransition(opacity: animation, child: child),
-          child: _showTitle
-              ? const Text(
-                  'Energy Audit Dashboard',
-                  key: ValueKey("title"),
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                )
-              : const SizedBox(height: 20,),
+        drawer: Drawer(
+          child: SideMenuWidget(
+            currentIndex: 0,
+            onMenuSelect: (index) {
+              print('Selected menu index: $index');
+            },
+          ),
         ),
-        centerTitle: true,
-      ),
-      drawer: Drawer(
-        child: SideMenuWidget(
-          currentIndex: 0,
-          onMenuSelect: (index) {
-            print('Selected menu index: $index');
-          },
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Consumer<ReportDataProvider>(
-          builder: (context, reportData, child) {
-            return Column(
-              children: [
-                //SizedBox(height: 40,),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.5,
-                    child: GridView.builder(
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2, // Number of columns
-                        crossAxisSpacing: 8,
-                        mainAxisSpacing: 8,
-                        childAspectRatio:
-                            1.7, // Adjust this for card height/width ratio
+        body: SingleChildScrollView(
+          child: Consumer<ReportDataProvider>(
+            builder: (context, reportData, child) {
+              return Column(
+                children: [
+                  //SizedBox(height: 40,),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.5,
+                      child: GridView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2, // Number of columns
+                          crossAxisSpacing: 8,
+                          mainAxisSpacing: 8,
+                          childAspectRatio:
+                              1.5, // Adjust this for card height/width ratio
+                        ),
+                        // physics: const NeverScrollableScrollPhysics(),
+                        itemCount: cards.length,
+                        itemBuilder: (context, index) {
+                          final CardData card = cards[index];
+                          return CardPage(card: card);
+                        },
                       ),
-                      // physics: const NeverScrollableScrollPhysics(),
-                      itemCount: cards.length,
-                      itemBuilder: (context, index) {
-                        final CardData card = cards[index];
-                        return CardPage(card: card);
-                      },
                     ),
                   ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15.0),
-                  child: LineChartCard(),
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    BUTTONWIDGET(
-                      name: "Add to Report",
-                      color: Colors.red,
-                      additem: () {
-                        try {
-                          print(cards[2].value);
-                          reportData.addData("Room 01", cards);
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                                content: Text('Room 01 added to report')),
-                          );
-                        } catch (e) {
-                          // Check if the card values are properly updated
-
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                                content:
-                                    Text('Failed to add Room 01 to report')),
-                          );
-                        }
-                      },
-                    ),
-                    BUTTONWIDGET(
-                      name: "New Task",
-                      color: Colors.green,
-                      additem: () {},
-                    ),
-                  ],
-                ),
-              ],
-            );
-          },
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: LineChartCard(),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      BUTTONWIDGET(
+                        name: "Add to Report",
+                        color: Colors.red,
+                        additem: () {
+                          try {
+                            print(cards[2].value);
+                            reportData.addData("Room 01", cards);
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content: Text('Room 01 added to report')),
+                            );
+                          } catch (e) {
+                            // Check if the card values are properly updated
+      
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content:
+                                      Text('Failed to add Room 01 to report')),
+                            );
+                          }
+                        },
+                      ),
+                      BUTTONWIDGET(
+                        name: "New Task",
+                        color: Colors.green,
+                        additem: () {},
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
